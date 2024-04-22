8cc2f62814e27340fb63f703b69bcf47be730137
EB-Merrimack <144274749+EB-Merrimack@users.noreply.github.com>
2024-04-22 10:14:32 -0400

    testing out netbeans

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,28 @@
+package com.mycompany.mavenproject1;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author emily
+ */
+public class Mavenproject1 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello World!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setTitle("JavaFX Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
