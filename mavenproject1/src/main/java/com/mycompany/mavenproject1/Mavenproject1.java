74950f3842bada1cc9db32eee8cf6d44776f30b5
EB-Merrimack <144274749+EB-Merrimack@users.noreply.github.com>
2024-04-27 11:20:13 -0400

    welcome screen initialized

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -3,6 +3,8 @@ package com.mycompany.mavenproject1;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,10 +16,26 @@ public class Mavenproject1 extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Label label = new Label("Hello World!!!!!!");
-        StackPane root = new StackPane(label);
+        // Create a label
+        Label label = new Label("");
+
+        // Create an ImageView to display the image
+        Image image = new Image("https://www.merrimack.edu/wp-content/uploads/DSC8310-2-scaled.jpg");
+        ImageView imageView = new ImageView(image);
+
+        // Bind fit width and height properties to the scene width and height
+        imageView.fitWidthProperty().bind(primaryStage.widthProperty());
+        imageView.fitHeightProperty().bind(primaryStage.heightProperty());
+
+        // Create a StackPane to hold the label and the image
+        StackPane root = new StackPane();
+        root.getChildren().addAll(imageView, label);
+
+        // Create a Scene
         Scene scene = new Scene(root, 300, 200);
-        primaryStage.setTitle("JavaFX Hello World");
+
+        // Set the scene and show the stage
+        primaryStage.setTitle("Merrimack Computer Science Campus Tour");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
