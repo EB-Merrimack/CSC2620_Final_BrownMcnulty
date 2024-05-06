5fc15b1337a27fd617199cf03d8a5923d281f83e
EB-Merrimack <144274749+EB-Merrimack@users.noreply.github.com>
2024-05-06 15:50:22 -0400

    its royal blue with one button on each side and blur transition

--- mavenproject1/src/main/java/module-info.java
+++ mavenproject1/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.mycompany.mavenproject1 {
     requires javafx.fxml;
     requires javafx.graphics;
     requires org.json;
+    requires java.desktop;
 
     opens com.mycompany.mavenproject1 to javafx.fxml;
     exports com.mycompany.mavenproject1;
