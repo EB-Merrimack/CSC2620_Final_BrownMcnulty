f0a30965f952ea5284b60a97ee0af736e5e35423
EB-Merrimack <144274749+EB-Merrimack@users.noreply.github.com>
2024-05-05 22:21:03 -0400

    button initate the building info

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/BuildingOptionsGUI.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/BuildingOptionsGUI.java
@@ -4,7 +4,8 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.effect.Glow;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
@@ -19,14 +20,23 @@ public class BuildingOptionsGUI extends Application {
     @Override
     public void start(Stage primaryStage) {
         // Create checkboxes for Main Campus and East Campus
-        CheckBox mainCampusCheckbox = new CheckBox("Main Campus");
-        CheckBox eastCampusCheckbox = new CheckBox("East Campus");
+        RadioButton mainCampusCheckbox = new RadioButton("Main Campus");
+        RadioButton eastCampusCheckbox = new RadioButton("East Campus");
+
+        // Create a ToggleGroup to ensure only one radio button can be selected at a time
+        ToggleGroup toggleGroup = new ToggleGroup();
+        mainCampusCheckbox.setToggleGroup(toggleGroup);
+        eastCampusCheckbox.setToggleGroup(toggleGroup);
 
         // Create a VBox to hold the checkboxes
         VBox checkboxBox = new VBox(10);
         checkboxBox.setAlignment(Pos.CENTER);
         checkboxBox.setPadding(new Insets(20));
         checkboxBox.getChildren().addAll(mainCampusCheckbox, eastCampusCheckbox);
+
+        // Decorator objects
+        BuildingInfo mainCampusInfo = new MainCampusInfo();
+        BuildingInfo eastCampusInfo = new EastCampusInfo();
 
         // Create placeholder text
         Text placeholderText = new Text("Click which campus you would like to learn about first");
@@ -45,6 +55,19 @@ public class BuildingOptionsGUI extends Application {
         placeholderPane.getChildren().add(placeholderText);
         placeholderPane.setStyle("-fx-background-color: royalblue; -fx-background-radius: 10px; -fx-padding: 10px;");
 
+        // Add event handlers for checkboxes
+        mainCampusCheckbox.setOnAction(e -> {
+            if (mainCampusCheckbox.isSelected()) {
+                placeholderText.setText(new BuildingInfoDecorator(mainCampusInfo).getInfo());
+            }
+        });
+
+        eastCampusCheckbox.setOnAction(e -> {
+            if (eastCampusCheckbox.isSelected()) {
+                placeholderText.setText(new BuildingInfoDecorator(eastCampusInfo).getInfo());
+            }
+        });
+
         // Create a BorderPane to organize the layout
         BorderPane borderPane = new BorderPane();
         borderPane.setLeft(checkboxBox);
@@ -58,23 +81,6 @@ public class BuildingOptionsGUI extends Application {
         primaryStage.setTitle("Building Options");
         primaryStage.setScene(scene);
         primaryStage.show();
-
-        // Decorator objects
-        BuildingInfo mainCampusInfo = new MainCampusInfo();
-        BuildingInfo eastCampusInfo = new EastCampusInfo();
-
-        // Add event handlers for checkboxes
-        mainCampusCheckbox.setOnAction(e -> {
-            if (mainCampusCheckbox.isSelected()) {
-                System.out.println(new BuildingInfoDecorator(mainCampusInfo).getInfo());
-            }
-        });
-
-        eastCampusCheckbox.setOnAction(e -> {
-            if (eastCampusCheckbox.isSelected()) {
-                System.out.println(new BuildingInfoDecorator(eastCampusInfo).getInfo());
-            }
-        });
     }
 
     public static void main(String[] args) {
