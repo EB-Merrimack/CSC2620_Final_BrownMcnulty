6b685da577f4250049d1b586766b47a5c6bec677
mcnultye <157040192+mcnultye@users.noreply.github.com>
2024-05-06 22:12:27 -0400

    Java docs

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/ImageWithCaptions.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/ImageWithCaptions.java
@@ -2,26 +2,50 @@ package com.mycompany.mavenproject1;
 
 import javafx.scene.image.Image;
 
+/**
+ * Represents an image with an associated caption.
+ */
 public class ImageWithCaptions {
-    private Image image;
-        private String caption;
-        private String imageUrl;
+    private Image image; // The image
+    private String caption; // The cpation associated with the image
+    private String imageUrl; // The URL of the image
 
-        public ImageWithCaptions(String imageUrl, String caption) {
-            this.image = new Image(imageUrl);
-            this.caption = caption;
-            this.imageUrl = imageUrl;
-        }
+    /**
+     * Constructs an ImageWithCaptions object with the specified image URL and caption.
+     * 
+     * @param imageUrl The URL of the image.
+     * @param caption The caption associated with the image.
+     */
+    public ImageWithCaptions(String imageUrl, String caption) {
+        this.image = new Image(imageUrl);
+        this.caption = caption;
+        this.imageUrl = imageUrl;
+    }
 
-        public Image getImage() {
-            return image;
-        }
+    /**
+     * Gets the image.
+     * 
+     * @return The image.
+     */
+    public Image getImage() {
+        return image;
+    }
 
-        public String getCaption() {
-            return caption;
-        }
+    /**
+     * Gets the caption associated with the image.
+     * 
+     * @return The caption.
+     */
+    public String getCaption() {
+        return caption;
+    }
 
-        public String getUrl() {
-            return imageUrl;
-        }
+     /**
+     * Gets the URL of the image.
+     * 
+     * @return The URL of the image.
+     */
+    public String getUrl() {
+        return imageUrl;
+    }
 }
